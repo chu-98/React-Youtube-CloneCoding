8dec7d81ecfbd6ebbfe02b077a3d77356b6d7387
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-04-14 19:11:27 +0900

    Popular 목록 API 이용해서 받아온 뒤, 보여주기

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import "./app.css";
+import VideoList from "./compoments/video_list/video_list";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAbecGEB1yQG7jIvGlbWzF6tpME3gHSRZ0",
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log("error", error));
+  }, []);
+  console.log(videos);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
-
-// API 키 설정하기
-// Postman 소개와 설정
